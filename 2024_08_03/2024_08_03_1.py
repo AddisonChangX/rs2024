a49cb96ba7db6f9c1adba99e7d08d47f294d3108
addisonchang0130 <addison13@msn.com>
2024-08-03 10:28:51 +0800

    Modify

--- 2024_08_03/2024_08_03_1.py
+++ 2024_08_03/2024_08_03_1.py
@@ -3,11 +3,11 @@ import os
 from datetime import datetime
 import random
 
-def main():
+def created_log_file():
     current_path = os.path.abspath(__name__)  #取得目前檔案路徑
     directory_name = os.path.dirname(current_path) #取得目前資料夾路徑
     data_path = os.path.join(directory_name,'data') #目前資料夾路徑加上data目錄
-    print(data_path)
+    
     if not os.path.isdir(data_path):
         print("沒有data的目錄，手動建立目錄")
         os.mkdir(data_path)
@@ -24,19 +24,25 @@ def main():
     else:
         print("有log檔")
 
+    return log_path
+    
+
+def record_info(log_path):
+    data_path = created_log_file()
+    now = datetime.now()
+    now_str = now.strftime("%Y-%m-%d %H:%M:%S")    
     humidity = str(random.randint(330,820) / 10)
     #humidity = str(random.uniform(33.0,82.0))
-
-
     celsius = str(random.randint(50,400) / 10)
     #celsius = str(random.uniform(5.0,40.0))
 
-
-    now = datetime.now()
-    now_str = now.strftime("%Y-%m-%d %H:%M:%S")
-
     with open(log_path,mode='a',encoding='utf-8',newline='') as file:
         file.write(now_str + ',' + humidity + ',' + celsius +'\n')
+
+
+def main():
+    log_path = created_log_file()
+    record_info(log_path)
 
 if __name__ == '__main__':
     main()
